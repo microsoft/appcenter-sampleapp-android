8030cef4008567e6ac7828f33dc2b7a11102b45c
sabrinaj206 <sabrinajiang@Sabrinas-MacBook-Pro.local>
2017-07-25 11:54:34 -0700

    running tests

--- app/src/androidTest/java/com/example/mobilecenter/sampleapp_android/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/mobilecenter/sampleapp_android/ExampleInstrumentedTest.java
@@ -4,10 +4,16 @@ import android.content.Context;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
 
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static org.junit.Assert.*;
+
+import android.support.test.runner.AndroidJUnit4;
+
+import com.xamarin.testcloud.espresso.Factory;
+import com.xamarin.testcloud.espresso.ReportHelper;
 
 /**
  * Instrumentation test, which will execute on an Android device.
@@ -16,11 +22,13 @@ import static org.junit.Assert.*;
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
+    @Rule
+    public ReportHelper reportHelper = Factory.getReportHelper();
+
     @Test
     public void useAppContext() throws Exception {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
-
-        assertEquals("com.example.tina.sampleapp_android", appContext.getPackageName());
+        assertEquals("com.example.mobilecenter.sampleapp_android", appContext.getPackageName());
     }
 }
