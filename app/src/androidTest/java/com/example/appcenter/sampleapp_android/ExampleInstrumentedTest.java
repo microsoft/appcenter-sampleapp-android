23b84f64e12eab4ce2967294b13664d9f83c78ce
Anna Kocheshkova <v-annkoc@microsoft.com>
2018-10-25 14:07:56 +0300

    Fix espresso tests

--- app/src/androidTest/java/com/example/appcenter/sampleapp_android/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/appcenter/sampleapp_android/ExampleInstrumentedTest.java
@@ -3,8 +3,11 @@ package com.example.appcenter.sampleapp_android;
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
+import com.xamarin.testcloud.espresso.Factory;
+import com.xamarin.testcloud.espresso.ReportHelper;
 
 import org.junit.Test;
+import org.junit.Rule;
 import org.junit.runner.RunWith;
 
 import static org.junit.Assert.*;
@@ -16,6 +19,10 @@ import static org.junit.Assert.*;
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
+
+    @Rule
+    public ReportHelper reportHelper = Factory.getReportHelper();
+
     @Test
     public void useAppContext() throws Exception {
         // Context of the app under test.
