d6c5df27756d8707ccf9fda2578383cd79912791
Neeraj Sewani <neerajsewanisrt8@gmail.com>
2018-10-28 00:08:20 +0530

    trash commit

--- app/src/main/java/com/example/appcenter/sampleapp_android/MainActivity.java
+++ app/src/main/java/com/example/appcenter/sampleapp_android/MainActivity.java
@@ -1,8 +1,8 @@
 package com.example.appcenter.sampleapp_android;
 
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
