ac85ee102e4c4e1befa97d1fbb674dad0b06aaf6
Vyas Nellutla <vyasknellutla@gmail.com>
2019-12-31 17:34:24 -0600

    Update azure-pipelines.yml for Azure Pipelines (#42)
    - Sign APK
    
    - Added App Center Azure DevOps Tasks
    
    - Prepared Espresso Tests for Upload
    
    Doc: https://docs.microsoft.com/en-us/appcenter/test-cloud/preparing-for-upload/espresso

--- app/src/androidTest/java/ms/appcenter/sampleapp/android/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/ms/appcenter/sampleapp/android/ExampleInstrumentedTest.java
@@ -5,6 +5,11 @@ import android.content.Context;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.platform.app.InstrumentationRegistry;
 
+import org.junit.Rule;
+import org.junit.After;
+import com.microsoft.appcenter.espresso.Factory;
+import com.microsoft.appcenter.espresso.ReportHelper;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -17,6 +22,14 @@ import static org.junit.Assert.assertEquals;
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
+    @Rule
+    public ReportHelper reportHelper = Factory.getReportHelper();
+
+    @After
+    public void TearDown() {
+        reportHelper.label("Stopping App");
+    }
+
     @Test
     public void useAppContext() {
         // Context of the app under test.
