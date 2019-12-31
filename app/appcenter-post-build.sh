947deaccb058c07f8b366d8051c726add371bae3
Vyas Nellutla <vyasknellutla@gmail.com>
2019-12-31 17:54:00 -0600

    Added App Center Build Scripts (#44)

---
+++ app/appcenter-post-build.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+echo "This is an App Center Post-Build script. For more information on how to use App Center build scripts vist: https://docs.microsoft.com/en-us/appcenter/build/custom/scripts"
